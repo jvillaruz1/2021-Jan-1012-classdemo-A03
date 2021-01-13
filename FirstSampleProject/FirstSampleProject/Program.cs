595f2402df1971b83becfcf2d69db795d71c0749
jvillaruz1 <77073031+jvillaruz1@users.noreply.github.com>
2021-01-13 10:16:22 -0700

    create first sample console program

---
+++ FirstSampleProject/FirstSampleProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstSampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How to:\t Withdraw in an ATM Machine");
+            Console.WriteLine("Go to an ATM Machine");
+            Console.WriteLine("Insert Card in the Card Slot");
+            Console.WriteLine("Enter Pin");
+            Console.WriteLine("Tap Withdraw");
+            Console.WriteLine("Select Chequing/Savings");
+            Console.WriteLine("Enter Withdraw amount");
+            Console.WriteLine("Retrieve Cash in the Cash Slot");
+            Console.WriteLine("Retrieve Card in the Card Slot");
+            Console.WriteLine("Retrieve Receipt in the Receipt Slot");
+        }
+    }
+}
