b3cfad8a9be60b2ca5261c03753199ac3ee93259
jvillaruz1 <77073031+jvillaruz1@users.noreply.github.com>
2021-01-13 10:29:21 -0700

    FirstSampleSolution

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the WriteLine() command will output the string to the console window.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This course sections is:\t OA03");
+            Console.WriteLine("Your instructor name is:\t Don Welch");
+            Console.WriteLine("I would like to:\t Learn C#");
+        }
+    }
+}
